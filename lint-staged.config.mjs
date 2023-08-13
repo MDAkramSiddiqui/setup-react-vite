fd4be84eb26bc5ff72fc11a5c4eb88351be93f9d
MDAkramSiddiqui <mohammadakram077@gmail.com>
2023-08-13 13:07:38 +0530

    chore: setup

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,4 +1,4 @@
 export default {
     '*.{mjs,cjs,js,jsx,ts,tsx}': ['prettier --write', 'pnpm run lint'],
-    '*.{css,scss,html,json,md}': ['prettier --write'],
+    '*.{css,scss,html,json,md,mdx,yaml,yml}': ['prettier --write'],
 };
